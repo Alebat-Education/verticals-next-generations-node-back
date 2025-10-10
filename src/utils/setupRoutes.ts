2467eb8aedb208588f6015b14e78924a938f69d2
rodri542 <yiyi.rodrigo@hotmail.com>
2025-10-10 15:21:44 -0600

    refactor(utils,config): integrate logger for standardized error and success messages

--- src/utils/setupRoutes.ts
+++ src/utils/setupRoutes.ts
@@ -1,5 +1,7 @@
 import { ERROR_SETTING_UP_ROUTES } from '@constants/errors/server.js';
 import { type Express } from 'express';
+import { FatalError } from '@constants/errors/errors.js';
+import { logger } from '@config/logger.js';
 
 export async function setupRoutes(app: Express): Promise<void> {
   const PRODUCTION_ROUTES = {
@@ -17,6 +19,8 @@ export async function setupRoutes(app: Express): Promise<void> {
   try {
     app.use(PATHS.PRODUCTS, (await PRODUCTION_ROUTES.PRODUCTS()).default);
   } catch (error) {
-    throw new Error(`${ERROR_SETTING_UP_ROUTES}: ${error}`);
+    const errorMessage = error instanceof Error ? error.message : String(error);
+    logger.fatal(`${ERROR_SETTING_UP_ROUTES}: ${errorMessage}`);
+    throw new FatalError(ERROR_SETTING_UP_ROUTES, error instanceof Error ? error : undefined);
   }
 }
