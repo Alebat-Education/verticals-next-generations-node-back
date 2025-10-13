8e91d7032d4e49a5ac57024af3ff4d827d3c4a27
Hiram Cornejo <131792188+soloyoloproXHD@users.noreply.github.com>
2025-10-12 20:51:27 -0600

    feat(app): add API rate limiter middleware to enhance request handling

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import { SERVER_CONFIG, SERVER_MESSAGES } from '@constants/common/server.js';
 import cors from 'cors';
 import { httpLogger } from '@config/logger.js';
 import morgan from 'morgan';
+import { apiRateLimiter } from '@middleware/rateLimiter.js';
 
 const app: Express = express();
 app.use(httpLogger);
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
+app.use(apiRateLimiter);
 
 app.get(SERVER_CONFIG.HOME, (_req: Request, res: Response) => {
   res.send(SERVER_MESSAGES.READY);
